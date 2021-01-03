49432fa4709cb13546765585a10fec370a5f85f2
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-01-03 15:43:39 -0300

    add titTacToeStruct

--- web/src/components/ScreenGame/index.tsx
+++ web/src/components/ScreenGame/index.tsx
@@ -1,13 +1,29 @@
 import { FC, useEffect, useRef } from 'react';
+import ticTacToeStruct from '../../utils/ticTacToeStruct';
 
 const ScreenGame: FC = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   useEffect(() => {
     const context = canvasRef.current?.getContext('2d');
-
-    context?.fillRect(100, 50, 100, 100);
+    ticTacToeStruct.forEach((array, indexArray) => {
+      array.forEach((value, index) => {
+        context?.fillRect(
+          (index + 1) * 200 + index * 10,
+          indexArray * 200 + indexArray * 10,
+          200,
+          200
+        );
+      });
+    });
   }, []);
-  return <canvas ref={canvasRef} width="1366" height="720" />;
+  return (
+    <canvas
+      onClick={(e) => console.log(e)}
+      ref={canvasRef}
+      width="1366"
+      height="720"
+    />
+  );
 };
 
 export default ScreenGame;
