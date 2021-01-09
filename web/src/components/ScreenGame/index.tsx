fbe493669c197f59370f23d1aaef73e6d4bc45e9
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-01-09 10:20:57 -0300

    add style of x and circle fill

--- web/src/components/ScreenGame/index.tsx
+++ web/src/components/ScreenGame/index.tsx
@@ -10,8 +10,10 @@ import ticTacToeStruct, { waysToWin } from '../../utils/ticTacToeStruct';
 
 const ScreenGame: FC = () => {
   const [matriz, setMatriz] = useState(3);
-  const [row, setRow] = useState([4]);
-  const [blocks, setBlocks] = useState<Array<number[]>>([[0, 0]]);
+  const [blocks, setBlocks] = useState<Array<number[]>>([]);
+  const [results, setResults] = useState<SetStateAction<Array<string>>>(
+    ticTacToeStruct
+  );
   const [chara, setChara] = useState('x');
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
@@ -52,6 +54,7 @@ const ScreenGame: FC = () => {
 
   useEffect(() => {
     const context = canvasRef.current?.getContext('2d');
+    const newBlocks: Array<[number, number]> = [];
     if (context?.fillStyle) {
       context.fillStyle = '#4B4B4B';
     }
@@ -61,119 +64,69 @@ const ScreenGame: FC = () => {
     for (let i = 0; i <= matriz - 2; i++) {
       fillRow(495, 260 + i * 100, context, 'horizontal');
     }
-    for (let i = 0; i <= matriz * matriz; i++) {
-      setBlocks((props) => [...props, [495 + i * 100, 170 * i * 100]]);
+    for (let i = 0; i < matriz; i++) {
+      const array: string[] = [];
+      for (let j = 0; j < matriz; j++) {
+        newBlocks.push([495 + i * 100 + 10, 170 + j * 100]);
+      }
     }
-  }, []);
+    setBlocks(newBlocks);
+  }, [matriz]);
 
-  // useEffect(() => {
-  //   const context = canvasRef.current?.getContext('2d');
-  //   if (context?.fillStyle) {
-  //     context.fillStyle = '#4B4B4B';
-  //   }
-  //   context?.fillRect(
-  //     0,
-  //     0,
-  //     Number(canvasRef.current?.width),
-  //     Number(canvasRef.current?.height)
-  //   );
+  function fillX(x: number, y: number): void {
+    const context = canvasRef.current?.getContext('2d');
 
-  //   matriz.forEach((array, indexArray) => {
-  //     array.forEach((value, index) => {
-  //       context?.clearRect(
-  //         (index + 1) * 200 + index * 10,
-  //         indexArray * 200 + indexArray * 10,
-  //         200,
-  //         200
-  //       );
-  //       if (value === 'o') {
-  //         context?.beginPath();
-  //         context?.arc(
-  //           (index + 1) * 200 + index * 10 + 100,
-  //           indexArray * 200 + indexArray * 10 + 100,
-  //           90,
-  //           0,
-  //           360
-  //         );
-  //         context?.stroke();
-  //       } else if (value === 'x') {
-  //         context?.beginPath();
-  //         context?.lineTo(
-  //           (index + 1) * 200 + (index + 1) * 10,
-  //           indexArray * 200 + indexArray * 10 + 10
-  //         );
-  //         context?.lineTo(
-  //           (index + 2) * 200 - (index + 1) * 10,
-  //           (indexArray + 1) * 200 - (indexArray + 1 * 10)
-  //         );
+    context?.beginPath();
+    context?.lineTo(x + 20, y + 20);
+    context?.lineTo(x + 70, y + 70);
 
-  //         context?.stroke();
+    context?.stroke();
 
-  //         context?.beginPath();
-  //         context?.moveTo(
-  //           (index + 2) * 200 - (index + 1) * 10,
-  //           indexArray * 200 + indexArray * 10 + 10
-  //         );
-  //         // context?.lineTo(
-  //         //   (index + 1) * 200 + (index + 1) * 10,
-  //         //   indexArray * 200 + indexArray * 10 + 10
-  //         // );
-  //         // context?.lineTo(
-  //         //   (index + 1) * 200 + (index + 1) * 10,
-  //         //   indexArray * 200 + indexArray * 10 + 10
-  //         // );
-  //         context?.lineTo(
-  //           (index + 1) * 200 + (index + 1) * 10,
-  //           (indexArray + 1) * 200 - (indexArray + 1 * 10)
-  //         );
+    context?.beginPath();
+    context?.moveTo(x + 70, y + 20);
+    context?.lineTo(x + 20, y + 70);
 
-  //         context?.stroke();
-  //       }
-  //     });
-  //   });
+    context?.stroke();
+  }
 
-  //   const win = matriz.every((array, indexarray) => {
-  //     return array.every((value, index) => {
-  //       if (waysToWin[indexarray][index] === '1') {
-  //         return true;
-  //       }
-  //       return value === waysToWin[indexarray][index];
-  //     });
-  //   });
+  function fillCircle(x: number, y: number): void {
+    const context = canvasRef.current?.getContext('2d');
+    context?.moveTo(x, y);
+    context?.beginPath();
+    context?.arc(x + 40, y + 40, 40, 0, Math.PI * 2);
 
-  //   if (win) {
-  //     console.log('voce vencer');
-  //   }
-  // }, [matriz]);
+    context?.stroke();
+  }
 
   function clickBlock(e: MouseEvent<HTMLCanvasElement>): void {
-    // const blocksAux = [...matriz];
-    // const newBlocks = blocksAux.map((row, indexArray) => {
-    //   const array = row.map((value, index) => {
-    //     if (
-    //       (index + 1) * 200 + index * 10 < e.clientX &&
-    //       (index + 2) * 200 + index * 10 > e.clientX &&
-    //       (indexArray + 1) * 200 + indexArray * 10 > e.clientY &&
-    //       indexArray * 200 + indexArray * 10 < e.clientY
-    //     ) {
-    //       return chara;
-    //     }
-    //     return value;
-    //   });
-
-    //   return array;
-    // });
-
-    // setBlocks(newBlocks);
-    console.log('oi');
+    console.log(results);
+    for (let i = 0; i < matriz * matriz; i++) {
+      if (
+        blocks[i][0] <= e.clientX &&
+        blocks[i][0] + 100 >= e.clientX &&
+        blocks[i][1] <= e.clientY &&
+        blocks[i][1] + 90 >= e.clientY
+      ) {
+        if (chara === 'x') {
+          // const newResults = results.map((blocks, index) => {
+          //   if (index === i) {
+          //     return chara;
+          //   }
+          // });
+          fillX(blocks[i][0], blocks[i][1]);
+        } else {
+          fillCircle(blocks[i][0], blocks[i][1]);
+        }
+      }
+    }
   }
 
   return (
     <canvas
       onClick={(e) => clickBlock(e)}
       ref={canvasRef}
-      width="1366"
-      height="720"
+      width="1166"
+      height="520"
     />
   );
 };
