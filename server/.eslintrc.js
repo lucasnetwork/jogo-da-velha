eb9fed2c7df03f618be0ea0e457a65b5ceb5bd05
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-01-10 11:52:35 -0300

    add initial config for backend

---
+++ server/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier/@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'linebreak-style': 0,
+  },
+};
