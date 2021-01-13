995dde2015e703ef9d1ca76e44a85a927cf698cf
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-01-12 21:14:34 -0300

    add database file

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,14 @@
 import express, { Express } from 'express';
 import cors from 'cors';
-import mongoose from 'mongoose';
 
 import routes from './routes';
+
+import './database/index';
 
 class App {
   server: Express;
 
   constructor() {
-    mongoose.connect('', { useUnifiedTopology: true });
     this.server = express();
     this.middlewares();
     this.route();
--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,9 +1,15 @@
 import mongoose from 'mongoose';
 
 class DB {
+  mongoose = mongoose;
+
   constructor() {
     this.mongo();
   }
 
-  mongo() {}
+  mongo() {
+    this.mongoose.connect('', { useUnifiedTopology: true });
+  }
 }
+
+export default new DB();
