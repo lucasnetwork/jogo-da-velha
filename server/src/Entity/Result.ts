f6ed7d3edcc2b65d637f949b3885402d66c8831b
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2021-01-15 11:59:53 -0300

    add Result entity

--- server/src/Entity/Result.ts
+++ server/src/Entity/Result.ts
@@ -22,19 +22,13 @@ class Result {
       });
       this.winAux = winOrLose || null;
     });
-    if (this.winAux === false) {
-      return 2;
-    }
     if (this.winAux) {
-      return 1;
-    }
-    if (this.winAux === null) {
       return 0;
     }
     if (!existEmptyFields) {
-      return 3;
+      return 1;
     }
-    return 0;
+    return -1;
   }
 }
 
